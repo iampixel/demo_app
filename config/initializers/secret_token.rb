138b5e6f297957d29966b4d752046daa985a6a78
iampixel <iampixel0@gmail.com>
2013-05-22 06:31:10 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '1c6a0e2534c36a7fa5550a88b879de30ea7ecfb38dfcbd076de4a22f8cd0e4aa3417c7c331b5e4d8e6db5e6f9a81405c714f0ac495ffaf02cbdec49e4b8f8279'
